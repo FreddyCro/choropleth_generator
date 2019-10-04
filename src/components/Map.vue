e5a26993149c54aed2a30a250efed2193cf483d1
Freddy Yang <freddy10100@hotmail.com>
2019-10-05 00:50:35 +0800

    essential ui

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -3,74 +3,93 @@
     <section class="color-map-section control-container">
       <div class="control-section">
         <h3>圖層</h3>
-        <input type="radio" name="map-layer" disabled /> 全台灣(這份圖資沒有這層)
-        <input type="radio" name="map-layer" checked="true" /> 縣市
-        <input type="radio" name="map-layer" /> 鄉鎮
+        <input type="radio" name="map-layer" @change="handleChangeLayer(1)" disabled /> 全台灣(這份圖資沒有這層)
+        <input type="radio" name="map-layer" @change="handleChangeLayer(2)" checked="true" /> 縣市
+        <input type="radio" name="map-layer" @change="handleChangeLayer(3)" /> 鄉鎮
       </div>
       <div class="control-section">
         <div class="sub-section">
           <h3>上色方式</h3>
-          <input type="radio" name="color-method" checked="true" /> 選縣市->填數字
-          <input type="radio" name="color-method" /> 選縣市->填色碼
+          <input type="radio" name="color-method" @change="handleChangeDrawingType(1)" checked="true" /> 智慧輸入
+          <input type="radio" name="color-method" @change="handleChangeDrawingType(2)" /> 輸入色碼
+        </div>
+        <div class="sub-section">
+          <h3>輸入方式</h3>
+          <input type="radio" name="input-method" @change="handleChangeInputType(1)" checked="true" /> 選擇器
+          <input type="radio" name="input-method" @change="handleChangeInputType(2)" /> 文字檔
         </div>
         <h3>資料類型</h3>
-        <input type="radio" name="data-type" checked="true" /> Numeric
-        <input type="radio" name="data-type" /> Category
+        <input type="radio" name="data-type" @change="handleChangeDataType(1)" checked="true" /> Numeric
+        <input type="radio" name="data-type" @change="handleChangeDataType(2)" /> Category
       </div>
       <div class="control-section">
-        <div class="sub-section">
-          <h3>輸入範圍</h3>
-          <input type="text" /> min
-          <input type="text" /> max
+        <button class="controll-button fold-button" @click="hanldeSectionFold($event)">展開</button>
+        <!-- Numeric -->
+        <div v-if="controller.dataType === 1 && controller.drawingType === 1" class="sub-section">
+          <div class="sub-section-row">
+            <h3>輸入範圍</h3>
+            <input type="text" name="data-range" placeholder="min"/>
+            <input type="text" name="data-range" placeholder="max"/>
+            <h3>輸入顏色數量</h3>
+            <input type="number" name="color-num" min="1" value="5" v-model="colorParams.colorNum" /> (5~7為佳, by Gestalt)
+          </div>
+          <div class="sub-section-row">
+            <h3>輸入色階</h3>
+            <input
+              v-for="(item, index) in (+colorParams.colorNum)"
+              :key="index"
+              type="text"
+              name="color-code"
+              placeholder="#fff"
+            />
+          </div>
         </div>
-        <div class="sub-section">
-          <h3>輸入顏色數量</h3>
-          <input type="number" value="5" /> (根據格式塔心理學 5~7為佳)
-        </div>
-        <div class="sub-section">
-          <h3>輸入色階<button>展開</button></h3>
-          <input type="text" /> color 1
-          <input type="text" /> color 2
-          <input type="text" /> color 3
+        <!-- Gatogory -->
+        <div v-if="controller.dataType === 2 && controller.drawingType === 1" class="sub-section">
+          <h3>輸入類別</h3>
+          <input type="text" name="data-catogory" v-model="colorParams.dataCatogoryInput"/> (A,B,C)
+          <h3>輸入色階</h3>
+          <input
+            v-for="(item, index) in computeDataCatogory"
+            :key="index"
+            type="text"
+            name="color-code"
+            placeholder="#fff"
+          />
         </div>
         <div class="sub-section">
           <h3>色碼</h3>
-          <input type="text" />
-          <input type="checkbox" /> 縣市名稱
+          <input type="text" name="specific-color-code" v-model="colorParams.specificColor" placeholder="#000" /><button>清除</button>
         </div>
       </div>
-      <div class="control-section">
-        <h3>選縣市<button>展開</button>
-        </h3>
-        <select>
-          <option>台北</option>
-          <option>新北</option>
-          <option>基隆</option>
+      <div v-if="controller.inputType === 1" class="control-section">
+        <button class="controll-button fold-button" @click="hanldeSectionFold($event)">展開</button>
+        <h3>選縣市</h3>
+        <select v-model="onSelectedCity">
+          <option v-for="(item, index) in cityObject" :key="index" :value="index">{{index}}</option>
         </select>
-        <div class="input-group">
-          <input type="checkbox" name="area" /> 中正區
+        <button name="controll-button" @click="handleSelectAllArea">全選</button>
+        <div
+          class="input-group"
+          v-for="(item, index) in cityObject[onSelectedCity]" :key="index"
+        >
+          <input type="checkbox" name="area" :value="item" v-model="onSelectedArea" /> {{item}}
+          <input v-if="controller.drawingType === 1" type="text" name="use-color-code" :placeholder="computeUseColorPlaceholder" />
         </div>
-        <div class="input-group">
-          <input type="checkbox" name="area" /> 萬華區
-        </div>
-        <div class="input-group">
-          <input type="checkbox" name="area" /> 大安區
-        </div>
-        <input type="submit" value="上色" />
-        <input type="submit" value="清空選取" />
+        <button name="controll-button" @click="drawingColor">上色</button>
+        <button name="controll-button">清空選取</button>
       </div>
-      <div class="control-section">
-        <h3>
-          輸入資料
-          <button>展開</button>
-        </h3>
-        <textarea name id cols="30" rows="10"></textarea>
-        <input type="submit" value="Submit to me" />
+      <div v-if="controller.inputType === 2" class="control-section">
+        <button>展開</button>
+        <h3>輸入資料</h3>
+        <textarea name id cols="30" rows="5"></textarea>
+        <button name="controll-button" @click="drawingColor">上色</button>
       </div>
     </section>
     <section class="color-map-section map-container">
       <div class="map-section map-section-mayor">
-        <svg class="map" />
+        <button class="controll-button fold-button" @click="hanldeSectionFold($event)">展開</button>
+        <svg id="map" />
       </div>
       <div class="map-section map-section-minor">
         <h3>樣式</h3>
@@ -92,21 +111,475 @@ export default {
         height: '100%',
         stroke: '1px',
       },
+      controller: {
+        layer: 1,
+        dataType: 1,
+        drawingType: 1,
+        inputType: 1,
+      },
+      colorParams: {
+        colorNum: 5,
+        colorGroup: [],
+        colorScheme: [],
+        specificColor: null,
+        dataCatogoryInput: '',
+      },
+      onSelectedCity: '基隆市',
+      onSelectedArea: [],
+      cityObject: {
+        基隆市: [
+          '仁愛區',
+          '中正區',
+          '信義區',
+          '中山區',
+          '安樂區',
+          '七堵區',
+          '暖暖區',
+        ],
+        臺北市: [
+          '中山區',
+          '大安區',
+          '松山區',
+          '中正區',
+          '信義區',
+          '大同區',
+          '萬華區',
+          '士林區',
+          '北投區',
+          '內湖區',
+          '南港區',
+          '文山區',
+        ],
+        新北市: [
+          '新莊區',
+          '中和區',
+          '樹林區',
+          '永和區',
+          '五股區',
+          '蘆洲區',
+          '三峽區',
+          '板橋區',
+          '土城區',
+          '鶯歌區',
+          '三重區',
+          '五股區',
+          '泰山區',
+          '林口區',
+          '八里區',
+          '淡水區',
+          '三芝區',
+          '石門區',
+          '金山區',
+          '萬里區',
+          '汐止區',
+          '瑞芳區',
+          '貢寮區',
+          '平溪區',
+          '雙溪區',
+          '新店區',
+          '深坑區',
+          '坪林區',
+          '烏來區',
+        ],
+        桃園市: [
+          '中壢區',
+          '楊梅區',
+          '平鎮區',
+          '龜山區',
+          '桃園區',
+          '龍潭區',
+          '大溪區',
+          '蘆竹區',
+          '八德區',
+          '大園區',
+          '新屋區',
+          '觀音區',
+          '復興區',
+        ],
+        新竹市: [
+          '東區',
+          '香山區',
+          '北區',
+        ],
+        新竹縣: [
+          '竹東鎮',
+          '竹北市',
+          '寶山鄉',
+          '新埔鎮',
+          '湖口鄉',
+          '橫山鄉',
+          '芎林鄉',
+          '新豐鄉',
+          '關西鎮',
+          '北埔鄉',
+          '尖石鄉',
+          '峨眉鄉',
+          '五峰鄉',
+        ],
+        苗栗縣: [
+          '卓蘭鎮',
+          '通霄鎮',
+          '苗栗市',
+          '苑裡鎮',
+          '竹南鎮',
+          '三義鄉',
+          '後龍鎮',
+          '大湖鄉',
+          '頭份市',
+          '銅鑼鄉',
+          '公館鄉',
+          '泰安鄉',
+          '西湖鄉',
+          '造橋鄉',
+          '頭屋鄉',
+          '獅潭鄉',
+          '南庄鄉',
+          '三灣鄉',
+        ],
+        臺中市: [
+          '豐原區',
+          '大里區',
+          '太平區',
+          '東勢區',
+          '新社區',
+          '神岡區',
+          '石岡區',
+          '霧峰區',
+          '和平區',
+          '大肚區',
+          '大安區',
+          '大甲區',
+          '大雅區',
+          '沙鹿區',
+          '龍井區',
+          '梧棲區',
+          '清水區',
+          '后里區',
+          '外埔區',
+          '烏日區',
+          '潭子區',
+          '中區',
+          '東區',
+          '西區',
+          '南區',
+          '北區',
+          '西屯區',
+          '南屯區',
+          '北屯區',
+        ],
+        南投縣: [
+          '南投市',
+          '埔里鎮',
+          '草屯鎮',
+          '竹山鎮',
+          '集集鎮',
+          '名間鄉',
+          '鹿谷鄉',
+          '中寮鄉',
+          '魚池鄉',
+          '國姓鄉',
+          '水里鄉',
+          '信義鄉',
+          '仁愛鄉',
+        ],
+        彰化縣: [
+          '彰化市',
+          '鹿港鎮',
+          '員林市',
+          '伸港鄉',
+          '和美鎮',
+          '線西鄉',
+          '田中鎮',
+          '花壇鄉',
+          '福興鄉',
+          '芳苑鄉',
+          '大村鄉',
+          '社頭鄉',
+          '芬園鄉',
+          '永靖鄉',
+          '秀水鄉',
+          '二水鄉',
+          '埔心鄉',
+          '北斗鎮',
+          '埔鹽鄉',
+          '溪湖鎮',
+          '溪州鄉',
+          '二林鎮',
+          '田尾鄉',
+          '埤頭鄉',
+          '竹塘鄉',
+          '大城鄉',
+        ],
+        雲林縣: [
+          '斗六市',
+          '虎尾鎮',
+          '林內鄉',
+          '古坑鄉',
+          '斗南鎮',
+          '莿桐鄉',
+          '西螺鎮',
+          '北港鎮',
+          '水林鄉',
+          '麥寮鄉',
+          '土庫鎮',
+          '大埤鄉',
+          '崙背鄉',
+          '二崙鄉',
+          '元長鄉',
+          '四湖鄉',
+          '台西鄉',
+          '口湖鄉',
+          '東勢鄉',
+          '褒忠鄉',
+        ],
+        嘉義市: [
+          '東區',
+          '西區',
+        ],
+        嘉義縣: [
+          '民雄鄉',
+          '大林鎮',
+          '新港鄉',
+          '竹崎鄉',
+          '梅山鄉',
+          '太保市',
+          '水上鄉',
+          '中埔鄉',
+          '布袋鎮',
+          '番路鄉',
+          '朴子市',
+          '里山鄉',
+          '溪口鄉',
+          '鹿草鄉',
+          '六腳鄉',
+          '東石鄉',
+          '大埔鄉',
+          '義竹鄉',
+          '阿里山鄉',
+        ],
+        臺南市: [
+          '新營區',
+          '後壁區',
+          '鹽水區',
+          '白河區',
+          '柳營區',
+          '官田區',
+          '學甲區',
+          '將軍區',
+          '東山區',
+          '善化區',
+          '北門區',
+          '麻豆區',
+          '六甲區',
+          '佳里區',
+          '下營區',
+          '七股區',
+          '中西區',
+          '東區',
+          '南區',
+          '北區',
+          '安平區',
+          '安南區',
+          '大內區',
+          '西港區',
+          '新市區',
+          '安定區',
+          '山上區',
+          '玉井區',
+          '楠西區',
+          '南化區',
+          '左鎮區',
+          '仁德區',
+          '歸仁區',
+          '關廟區',
+          '龍崎嶇',
+          '永康區',
+        ],
+        高雄市: [
+          '鹽埕區',
+          '鼓山區',
+          '左營區',
+          '三民區',
+          '新興區',
+          '前金區',
+          '苓雅區',
+          '前鎮區',
+          '旗津區',
+          '小港區',
+          '鳳山區',
+          '林園區',
+          '大寮區',
+          '大樹區',
+          '大社區',
+          '仁武區',
+          '鳥松區',
+          '岡山區',
+          '橋頭區',
+          '燕巢區',
+          '田寮區',
+          '阿蓮區',
+          '路竹區',
+          '湖內區',
+          '茄萣區',
+          '永安區',
+          '彌陀區',
+          '梓官區',
+          '旗山區',
+          '美濃區',
+          '六龜區',
+          '甲仙區',
+          '杉林區',
+          '內門區',
+          '茂林區',
+          '桃源區',
+          '那瑪夏區',
+        ],
+        宜蘭縣: [
+          '宜蘭市',
+          '羅東鎮',
+          '蘇澳鎮',
+          '頭城鎮',
+          '礁溪鄉',
+          '壯圍鄉',
+          '員山鄉',
+          '冬山鄉',
+          '五結鄉',
+          '三星鄉',
+          '大同鄉',
+          '南澳鄉',
+        ],
+        花蓮縣: [
+          '花蓮市',
+          '秀林鄉',
+          '吉安鄉',
+          '玉里鎮',
+          '鳳林鎮',
+          '光復鄉',
+          '卓溪鄉',
+          '瑞穗鄉',
+          '萬榮鄉',
+          '富里鄉',
+          '新城鄉',
+          '壽豐鄉',
+          '豐濱鄉',
+        ],
+        臺東縣: [
+          '長濱鄉',
+          '臺東市',
+          '成功鎮',
+          '關山鎮',
+          '卑南鄉',
+          '大武鄉',
+          '太麻里鄉',
+          '東河鄉',
+          '鹿野鄉',
+          '池上鄉',
+          '綠島鄉',
+          '延平鄉',
+          '海端鄉',
+          '達仁鄉',
+          '金峰鄉',
+          '蘭嶼鄉',
+        ],
+        澎湖縣: [
+          '馬公市',
+          '湖西鄉',
+          '白沙鄉',
+          '西嶼鄉',
+          '望安鄉',
+          '七美鄉',
+        ],
+        金門縣: [
+          '金城鎮',
+          '金湖鎮',
+          '金沙鎮',
+          '金寧鄉',
+          '烈嶼鄉',
+          '烏坵鄉',
+        ],
+        連江縣: [
+          '南竿鄉',
+          '北竿鄉',
+          '莒光鄉',
+          '東引鄉',
+        ],
+      },
     };
   },
-  methods: {},
+  computed: {
+    computeUseColorPlaceholder() {
+      if (this.controller.dataType === 2) return 'ex: B';
+      if (this.controller.drawingType === 1) return 'ex: 50';
+      else if (this.controller.drawingType === 2) return 'ex: 3';
+      else return;
+    },
+    computeDataCatogory() {
+      if (this.colorParams.dataCatogoryInput) return this.colorParams.dataCatogoryInput.split(',');
+      else return 1;
+    }
+  },
+  watch: {
+    onSelectedCity: function() {
+      this.onSelectedArea = [];
+    },
+  },
+  methods: {
+    handleChangeLayer(index) {
+      this.controller.layer = index;
+    },
+    handleChangeDrawingType(index) {
+      this.controller.drawingType = index;
+    },
+    handleChangeDataType(index) {
+      this.controller.dataType = index;
+    },
+    handleChangeInputType(index) {
+      this.controller.inputType = index;
+    },
+    hanldeSectionFold: function(event) {
+      
+    },
+    handleSelectAllArea() {
+      this.onSelectedArea = this.cityObject[this.onSelectedCity].map(e => e);
+    },
+    drawingColor() {
+      const mapSvg = d3.select('#map');
+      switch (this.controller.inputType) {
+        case 1:
+          if (
+            this.colorParams.specificColor &&
+            this.onSelectedArea
+          ) {
+            this.onSelectedArea.forEach(e => {
+              mapSvg.select('#' + e)
+                .transition()
+                .duration(666)
+                .ease(d3.easeCubicIn)
+                .style('fill', this.colorParams.specificColor);
+            });
+          } else {
+            alert('error!');
+          }
+          break;
+        case 2:
+          
+          break;
+        default:
+          break;
+      }
+    },
+  },
   mounted: function() {
     let topojson = require('topojson');
-
     let mapSvg = d3
-      .select('.map')
+      .select('#map')
       .attr('width', this.mapConfig.width)
       .attr('height', this.mapConfig.height);
 
     let projection = d3
       .geoMercator()
-      .center([121.159531, 24.988567])
-      .scale(5500);
+      .center([121, 25.3])
+      .scale(5000);
 
     let path = d3.geoPath().projection(projection);
     let map = d3.json('./TOWN_MOI_1080726.json');
@@ -127,8 +600,10 @@ export default {
         .attr('class', (d) => {
           return d.properties.COUNTYNAME + ' ' + d.properties.TOWNNAME;
         })
-        .style('stroke', 'black')
-        .style('fill', 'none');
+        .attr('id', (d) => {
+          return d.properties.TOWNNAME;
+        })
+        .style('stroke', 'black');
 
       // All Taiwan, no counties layer.
       // mapSvg
@@ -149,13 +624,30 @@ export default {
   height: 100vh;
   display: flex;
   justify-content: center;
+  .fold-button {
+    position: absolute;
+    top: 0;
+    right: 0;
+  }
+  .fold-button-map {
+    position: absolute;
+    top: 0;
+    left: 0;
+  }
+  input[type="checkbox"] {
+    width: 15px;
+    height: 15px;
+  }
+  input[type="text"]{
+    width: 75px;
+  }
 }
 .color-map-section {
-  border: solid 1px;
   display: flex;
   flex-direction: column;
   justify-content: space-between;
   align-items: flex-start;
+  padding: 10px 5px;
   .map-section {
     position: relative;
     width: 100%;
@@ -182,6 +674,7 @@ export default {
   justify-content: space-between;
   align-items: flex-start;
   .control-section {
+    position: relative;
     width: 100%;
     height: 100%;
     border: solid 1px;
@@ -191,6 +684,7 @@ export default {
 .map-container {
   position: relative;
   width: 60%;
+  overflow: hidden;
 }
 .full-page {
   position: fixed;
@@ -201,6 +695,7 @@ export default {
 }
 path {
   stroke-width: 1px;
+  fill: #fff;
 }
 </style>
 
